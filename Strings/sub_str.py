98e4f8e75350915f68cad8e5d3159a0203d0a7fe
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-05-17 20:37:51 +0530

    Create sub_str.py

---
+++ Strings/sub_str.py
@@ -0,0 +1,11 @@
+'''
+Problem url - https://leetcode.com/problems/implement-strstr/submissions/
+'''
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle ==" ":
+            return (0)
+        elif needle in haystack:
+            return (haystack.index(needle))
+        else:
+            return (-1)
