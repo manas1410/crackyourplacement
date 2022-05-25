4cd608c70dcc16fd85e4cbefecedb85a62da65dc
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-05-25 16:06:54 +0530

    Create Valid_palindrome_ii.py

---
+++ Strings/Valid_palindrome_ii.py
@@ -0,0 +1,21 @@
+'''
+Problem url:- https://leetcode.com/problems/valid-palindrome-ii/submissions/
+'''
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def checkpalin(s,i,j):
+            while(i<j):
+                if s[i]!=s[j]:
+                    return False
+                i+=1
+                j-=1
+            return True
+        i = 0
+        j = len(s)-1
+        while(i<j):
+            if s[i] != s[j]:
+                return checkpalin(s,i,j-1) or checkpalin(s,i+1,j)
+            i+=1
+            j-=1
+        return True
